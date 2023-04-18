0a50912f5adeb85dd9bcf4e00b8dc74531171e3c
Kelvin Forde <kelvinforde100@gmail.com>
2023-04-18 01:00:39 +0100

    Both user login and new user creation api endpoints working with front end

--- password_manager_client/lib/main.dart
+++ password_manager_client/lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:password_manager_client/user_home.dart';
 import 'package:platform_device_id/platform_device_id.dart';
+import 'package:http/http.dart' as http;
+import 'package:status_alert/status_alert.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +14,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Password Manager',
-      home: const MyHomePage(title: 'Password Manager'),
+      home: MyHomePage(title: 'Password Manager'),
     );
   }
 }
@@ -26,9 +30,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final TextEditingController _userphone = TextEditingController();
-  String? _androidId;
+  late String _androidId;
 
   // function to get the android id of phone
   void getInfo() async {
@@ -36,7 +39,144 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // Update ID
     setState(() {
-      _androidId = result;
+      _androidId = result ?? "";
+    });
+  }
+
+  void userLogin() async {
+    bool account = false;
+    print("Running...");
+    String? base =
+        "https://passwordmanagerapiead.azurewebsites.net/PasswordManager/api/Users/find?phoneID=";
+
+    base = base + _androidId;
+    var uri = Uri.parse(base);
+
+    final response = await http.get(uri);
+    final body = response.body;
+    var json;
+
+    if (body == "No user found with that phone") {
+      account = false;
+    } else {
+      json = jsonDecode(body);
+      account = true;
+    }
+
+    setState(() {
+      if (!account) {
+        print("No account.");
+        showDialog(
+            context: context,
+            builder: ((context) => AlertDialog(
+                title: Text("New Account?"),
+                content: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.red[400],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Align(
+                            alignment: Alignment.center,
+                            child: Text("Cancel",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ))),
+                      ),
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                        createUser();
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.green[400],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Align(
+                            alignment: Alignment.center,
+                            child: Text("Create",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ))),
+                      ),
+                    ),
+                  ],
+                ))));
+      } else {
+        StatusAlert.show(
+          context,
+          duration: const Duration(seconds: 2),
+          subtitle: "Login Successful!",
+          configuration: const IconConfiguration(
+              icon: Icons.check_circle_rounded, color: Colors.green),
+          maxWidth: 250,
+        );
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => UserHomePage(
+                    androidId: json['phoneid'],
+                    userphone: _userphone.text.trim(),
+                  )),
+        );
+      }
+    });
+  }
+
+  void createUser() async {
+    bool created = false;
+
+    String? base =
+        "https://passwordmanagerapiead.azurewebsites.net/PasswordManager/api/Users/add?phoneID=";
+
+    base = base + _androidId;
+    var uri = Uri.parse(base);
+
+    final response = await http.post(uri);
+    final body = response.body;
+    print(body);
+
+    if (body != "User added") {
+      created = false;
+    } else {
+      created = true;
+    }
+
+    print(created);
+
+    setState(() {
+      if (created) {
+        StatusAlert.show(
+          context,
+          duration: const Duration(seconds: 2),
+          subtitle: "New User Created!",
+          configuration: const IconConfiguration(
+              icon: Icons.check_circle_rounded, color: Colors.green),
+          maxWidth: 250,
+        );
+        userLogin();
+      } else {
+        showDialog(
+            context: context,
+            builder: (context) =>
+                AlertDialog(title: Text("Error creating account.")));
+      }
     });
   }
 
@@ -114,7 +254,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Enter your phone number..',
-                    
                   ),
                 ),
               ),
@@ -123,14 +262,7 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: const EdgeInsets.only(top: 10),
               child: GestureDetector(
                 onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => UserHomePage(
-                          userphone: _userphone.text.trim(),
-                          androidId: _androidId,
-                        )),
-                  );
+                  userLogin();
                 },
                 child: Container(
                   height: 50,
